01315d8b72a30a6995f9e0073e8bbdbffb565185
thomasgrbic <grbic.t@northeastern.edu>
2024-05-20 13:31:15 -0400

    Up to Ticket 01

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    let mut n_mut = n;
+    while n_mut > 0 {
+        result *= n_mut;
+        n_mut -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
